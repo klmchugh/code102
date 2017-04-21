3b4aef4c02660fe25c538a10c2bd1e0fc07d4eb0
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-04-20 23:30:36 -0700

    modified code and notes files for thursday

--- code/thursday.js
+++ code/thursday.js
@@ -1,45 +1,40 @@
-var day = 'Thursday',
+var day = 'Friday',
     thursday = 'thursday';
 
-// How many equals signs does it take to screw your code?
-if (day = 'thursday') {
-  console.log('Test 1: Well lookee there, it\'s ' + day);
-}
-
-// The case of the failing "if" statement
-if (day == 'Thursday') {
-  console.log('Test 2: Lo and behold, it\'s ' + day);
-}
-
-// Equals thrice, test once
-if (day === 'Thursday') {
-  console.log('Test 3: Huh. It\'s ' + day);
-}
+// // How many equals signs does it take to screw your code?
+// if (day = 'thursday') {
+//   console.log('Test 1: Well lookee there, it\'s ' + day);
+// }
+//
+// // The case of the failing "if" statement
+// if (day == 'Thursday') {
+//   console.log('Test 2: Lo and behold, it\'s ' + day);
+// }
+//
+// // Equals thrice, test once
+// if (day === 'Thursday') {
+//   console.log('Test 3: Huh. It\'s ' + day);
+// }
 
 // TODO: After commenting out the 3 conditionals above,
 //       uncomment this code and run it.
-// if (day === 'Thursday') {
-//   console.log('Is it Friday yet?');
-// } else {
-//   console.log('Whistle while you work!')
-// }
 
-// TODO: Add an "else if" block to check whether it's Saturday
+// DONE TODO: Add an "else if" block to check whether it's Saturday
 //       and console.log() a different statement for the weekend.
 
 
 
-// TODO: Modify the conditional for Saturday to check whether
+// DONE TODO: Modify the conditional for Saturday to check whether
 //       the day is Saturday OR Sunday.
 
 
 
-// TODO: Add an "else if" block to check whether it's Monday
+// DONE TODO: Add an "else if" block to check whether it's Monday
 //       and console.log() a different statement for Monday.
 
 
 
-// TODO: Change the string that's assigned into the day
+// DONE TODO: Change the string that's assigned into the day
 //       variable and run the code several times.
 
 
@@ -54,7 +49,7 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // console.log('index 20: ' + days[20]);
 
 // TODO: DRY it up!
-// for (var i = 0; i < 7; i++) {
+// for (var i = 0; i < days.length; i++) {
 //   console.log('index ' + i + ': ' + days[i]);
 // }
 
@@ -65,8 +60,19 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // TODO: On each iteration, use our randomly generated dayNumber
 //       to get a day from the days array, and console.log the
 //       name of the day.
-// var max = 100,
-//     result;
-// for (var j = 0; j < max; j++) {
-//   dayNumber = Math.floor(Math.random() * days.length);
-// }
+var max = 100,
+    result;
+for (var j = 0; j < 1; j++) {
+  dayNumber = Math.floor(Math.random() * days.length);
+//  console.log(days[dayNumber]);
+}
+
+if (days[dayNumber] === 'Thursday') {
+  console.log('Is it Friday yet?');
+} else if (days[dayNumber] === 'Saturday' || days[dayNumber] === 'Sunday') {
+  console.log('Friday already happened, welcome to the weekend.');
+} else if (days[dayNumber] === 'Monday') {
+  console.log('Not even close.');
+} else {
+  console.log('Whistle while you work!');
+}
---
+++ notes/thursday-AMPotter.md
@@ -0,0 +1,14 @@
+remote (origin) is essentially an alias for the url to my copy of the repository.
+The branch is a bookmark for the repository. It's like which version you're working on, on a timeline. (?)
+
+expressions:
+
+"truthy and falsy":
+
+if/else:
+
+for loops:
+
+functions, named & anonymouse:
+
+IIFEs:
